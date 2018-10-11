5b7ecbb495a15a89d89bb21e22fc14b67d4febb0
Brandonlrs27 <43393536+Brandonlrs27@users.noreply.github.com>
2018-10-10 18:36:25 -0700

    EC3-1

---
+++ EC3-1/EC3-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC3_1
+{
+    class Program
+        //Lares Dominguez Brandon
+    {
+        static void Main(string[] args)
+        {
+            var Pila = new Stack();
+            Pila.Push("Brandon");
+            Pila.Push("Jonatan");
+            Pila.Push("Issac");
+            Pila.Push(444);
+            //Muestra los datos de la pila.
+
+            Console.WriteLine("La pila contiene :");
+            foreach(var valor in Pila)
+            {
+                Console.WriteLine(valor);
+            }
+
+            //Revisa si existe la cadena Issac.
+            Console.WriteLine("La pila contiene a Issac: ");
+            bool tiene = Pila.Contains("Issac");
+
+            Console.WriteLine(tiene); 
+
+            Console.WriteLine(Pila.Pop().GetType()); //Nos dice tipo de instancia actual.
+
+            Console.WriteLine(Pila.ToString()); //Devuelve los datos en un String.
+            Console.WriteLine(Pila.ToArray()); //Copia los datos de la pila a una nueva matriz.
+            Console.WriteLine(Pila.GetEnumerator()); //Utiliza enumeradores para recorrer la pila.
+            Console.ReadKey(); 
+                    
+                   
+        }
+    }
+}
